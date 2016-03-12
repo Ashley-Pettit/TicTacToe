cadb1a298570b8d6c2f800f22af2af7fc8e0f6be
Ashley-Pettit <Ashley@digital380.com.au>
2016-03-13 01:45:37 +1100

    Stable Version. logic for agressively playing hard ai. CSS tweaks

--- TicTacToe.js
+++ TicTacToe.js
@@ -34,12 +34,17 @@ function countdown_animation() {
 		$('#begun').delay(5560).fadeIn(1500); // in at 10,000 - Does not go out
 		$('#play_happening').delay(5570).fadeIn(1500); // in at 10,000 - Does not go out
 		$('.game_table').delay(5570).fadeIn(1500); // in at 10,000 - Does not go out
-		$('#play_again').delay(5570).fadeIn(1500);
         $('#whos_turn_is_it').delay(5570).empty().fadeIn(1500).prepend(current_player + " It's your turn");
 		$('#home').delay(5570).fadeIn(1500);
 		$('#score').delay(5570).fadeIn(1500);
-		$('#title_score1').prepend("Computer Score");
-		$('#title_score2').prepend("Human Score");
+		if (difficulty === "human") {
+			$('#title_score1').prepend("Human 1 Score");
+			$('#title_score2').prepend("Human 2 Score");
+		}
+		else {
+			$('#title_score1').prepend("Computer Score");
+			$('#title_score2').prepend("Human Score");
+		}
     //
     });
 }
@@ -164,6 +169,7 @@ function clear_board() {
     	$('#won').empty();
         is_round_in_progress = true;
 		change_starting_player();
+		$('#play_again').fadeOut(1000);
         for (var i = 0; i < 9; i++) { // Clearing the array 
             game_board[i] = null;
         }
@@ -226,6 +232,7 @@ function round_won() {
     update_score();
     update_board();
     AI_playing = false;
+    $('#play_again').fadeIn(1000);
 }
 
 
@@ -235,6 +242,7 @@ function round_drew() {
     is_round_in_progress = false;
     update_board();
     AI_playing = false;
+    $('#play_again').delay().fadeIn(1500);
 }
 
 
@@ -269,27 +277,77 @@ function AI_Intermediate() {
 
 
 function AI_hard_agressive() {
+// This AI occurs when hard is selected and the AI has the starting turn. As the starting player the computer agressively attempts to force 2 win scenarios. 
+
 	var AI_playing = true;
 	while (AI_playing) {
 	var random_move = Math.floor(Math.random() * 3); // The computer has 3 possible opening moves which are then rotated by a 25 degrees leading to 12 possible opening plays
-	// PLAY 1 - COMPUTER PLAYER A CORNER CELL
-	// PLAY 2 - COMPUTER PLAYS A SIDE CELL
-	// PLAY 3 - COMPUTER PLAYS IN MIDDLE
+	
+	// TURN 1
+
+	// Option 1-1 - COMPUTER PLAYER A CORNER CELL
+	// Option 1-2 - COMPUTER PLAYS A SIDE CELL
+	// Option 1-3 - COMPUTER PLAYS IN MIDDLE
 	var random_orientation = Math.floor(Math.random() * 4) // After finding out what type of cell to play. Then randomly orient the play
 	if (random_move = 1) { 
-		//If computer played on first turn on a corner cell it has three possible next moves. 
-		//OPTION 1 (Preferenced) - The computer will play in the center -  The computer will now win in 1-2 moves
-		//OPTION 2 - The computer will play either the side cell which is 3 units ahead of the current cell (Attempting to force a 2 way win situation) OR
-		//OPTION 3 - The computer plays in the opposite corner - Again attempting to try to force a 2 way win situation.  
-	}
-		//If the computer did option 1 
 
+	// TURN 3
+
+	//IF TURN 1 = OPTION 1  
+
+		//OPTION 1-1 - The computer will play in the center(Preferenced)
+		//OPTION 1-2 - The computer will play either the side cell which is 3 units ahead of the current cell (Attempting to force a 2 way win situation) OR
+		//OPTION 1-3 - The computer plays in the opposite corner - Again attempting to try to force a 2 way win situation.  
+		// NOTE - The computer does not play turn 2 in either of the matching side cells. This is considered a poor move (As their is less opportunity to win)
+		// Likewise playing either side cell next to the played corner cell is also considered a poor move.
+	}
+	// IF TURN 1 = OPTION 2
+
+	//OPTION (T3)2-1 - The computer takes the center (Preferenced)
+	//OPTION (T3)2-2 - The computer takes a corner cell which must be on the opposite side of the board. There are two of these. 
+
+	// IF TURN 1 = OPTION 3
+
+	// OPTION (T3)3-1 - The computer takes any free corner. There are 4. 
+	// Playing a side cell is considered a poor move. 
 
 	}
+
+	// TURN 5
+
+	// The computer attempts a win (Preferenced)
+	// The computer blocks a player win (2nd preferenced)
+
+// OTHER SCENARIOS
+
+// Has side and corner. if player plays in cells +1 or +2 of the corner cell the computer forces a two way win by playing -1 from the it's middle cell. 
+// HAS side and corner. Only 2 more situations exist which are player played in cells -1 or +1 of middle cell. No win is possible. Random play
+
+// Has corner and corner. The computer simply blocks any win condition of the player. The player will have win conditions in all directions. The computer must block them. If this leads to a corner play the
+// computer will win on TURN 7
+
+// Has corner and middle but can't win. The computer blocks any player win if it has to. 
+//After this there are 2 cells. They are -3 or + 1 of the corner cell. Playing either will result in a 2 way win condition. 
+ 
+//TURN 7
+
+// Very simple. The computer plays the remaining cell. 
+
 }
 
 
 function AI_hard_defending() {
+//This AI occurs when hard is selected yet the computer does not start the round. The computer will aim to draw the round. It will win if possible but it's key goal is to draw and wait for the next round. 
+
+// PLAY 1 
+
+// OPTION (T2)1-1 - The computer takes the center (If the center is not taken by the 2nd turn a loss is possible. Thus this move must be taken unless the player went centre) (Preferenced)
+// OPTION (T2)1-2 - If center is taken the computer takes any corner 
+// Note a side is considered a poor play
+
+// PLAY 2 
+
+// 
 
 }
 
