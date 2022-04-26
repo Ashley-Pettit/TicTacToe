ed8c00165e05514112127a82860f3cdf298587a9
UNSW Onboarding <Ashley@digital380.com.au>
2022-04-26 12:00:37 +1000

    Add files via upload

---
+++ ClearIECacheWindows10.bat
@@ -0,0 +1,21 @@
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255
+
+REM History:
+REM RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 1
+
+REM Cookies:
+REM RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2
+
+REM Temp Internet Files:
+REM RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+
+REM Form Data:
+REM RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 16
+
+REM Passwords:
+REM RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 32
+
+REM OR
+
+REM All:
+rundll32.exe InetCpl.cpl,ClearMyTracksByProcess 4351
